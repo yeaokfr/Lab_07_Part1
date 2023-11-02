7b8265728051e56a16602fe2ab4896ac6e454475
yeaokfr <144458128+yeaokfr@users.noreply.github.com>
2023-11-01 21:49:11 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 0; i <= 30; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int j = 30; j >= 0; j--) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+
+        for (int k = 0; k <= 18; k += 3) {
+            System.out.print(k + " ");
+
+        }
+        System.out.println();
+
+        for(int l = 10; l >= 0; l-= 2) {
+            System.out.print(l + " ");
+        }
     }
 }
